342594f03bc5d82ba2109f2be71975059991d5db
ceaser501 <ceaser501@naver.com>
2022-04-23 03:30:35 +0900

    Hash 문제풀이
    05. K번째 큰 수 해야함

---
+++ src/HashMap/HashMap_4_1_ClassPresident/Main.java
@@ -0,0 +1,46 @@
+package HashMap.HashMap_4_1_ClassPresident;
+
+// 해쉬_학급 회장
+// 입력 값 : BACBACCACCBDEDE
+// 출력 값 : C
+import java.util.*;
+
+public class Main {
+
+    public char solution(String s){
+        char answer = ' ';
+
+        /**
+         * 1. map.getOrDefault(key, defaultValue) : key에 따른 value를 가져오거나, 없으면 defaultValue를 가져온다
+         * 2. map.containsKey('A') : 해당 key 가 있는지 판별한다 (true/false)
+         * 3. map.size : map에 속한 key값의 개수를 출력한다
+         * 4. map.remove('A') : 해당 key-value set을 삭제한다
+         * 5. map.keySet() : for문을 돌 때, key를 돌린다
+         */
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char x : s.toCharArray()){
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(char key : map.keySet()){
+            if(map.get(key) > max){
+                max = map.get(key);             // public V get(Object key) 이므로 int로 자동 캐스팅
+                answer = key;
+            }
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String args[]){
+        Main T = new Main();
+
+        Scanner kb = new Scanner(System.in);
+        String s = kb.next();
+
+        System.out.println(T.solution(s));
+    }
+}
---
+++ src/HashMap/HashMap_4_4_FindAllAnagram_2/Main.java
@@ -0,0 +1,49 @@
+package HashMap.HashMap_4_4_FindAllAnagram_2;
+
+// 해쉬_모든 아나그램 찾기 (시간복잡도 O(n))
+// 최적화
+import java.util.*;
+
+public class Main {
+
+    /**
+     * 입력값 : bacaAacba
+     * 출력값 : abc
+     */
+    public int solution(String s, String t){
+        int answer = 0;
+
+        // 비교군 t에 해당하는 map 생성
+        HashMap<Character, Integer> tmap = new HashMap<>();
+        for(char c : t.toCharArray()){
+            tmap.put(c, tmap.getOrDefault(c, 0) + 1);
+        }
+
+        // 대상 s에 해당하는 map 생성 (단, t 길이보다 -1 만큼만 세팅)
+        HashMap<Character, Integer> smap = new HashMap<>();
+        for(int i=0; i<t.length()-1; i++){
+            smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i), 0)+1);         // 0번째, 1번째만 세팅
+        }
+
+        int lt=0;
+        for(int rt=t.length()-1; rt<s.length(); rt++){
+            smap.put(s.charAt(rt), smap.getOrDefault(s.charAt(rt), 0)+1);       // 2번째 세팅
+            if(smap.equals(tmap)) answer++;                                                // smap과 tmap을 비교 (equals는 key, value 모두 비교 됨)
+
+            smap.put(s.charAt(lt), smap.get(s.charAt(lt))-1);                       // 최초 lt 위치의 값을 -1 한다
+            if(smap.get(s.charAt(lt)) == 0) smap.remove(s.charAt(lt));                        // 값이 0인 lt key를 삭제
+            lt++;
+        }
+
+        return answer;
+    }
+    public static void main(String args[]){
+        Main T = new Main();
+
+        Scanner kb = new Scanner(System.in);
+        String s = kb.next();         // 대상 문자열
+        String t = kb.next();         // 찾을 문자열
+
+        System.out.println(T.solution(s, t));
+    }
+}
