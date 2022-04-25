9b4f5caf5dc0a8284871430bf0f8cf6c23eda92d
taesu.kim <ceaser501@naver.com>
2022-04-25 16:59:37 +0900

    진행중

---
+++ src/Stack_5_6_SaveThePrincess/Main.java
@@ -0,0 +1,26 @@
+package Stack_5_6_SaveThePrincess;
+
+// 큐_공주 구하기
+import java.util.*;
+
+public class Main {
+
+  public int solution(int prince, int k){
+    int answer = 0;
+
+    
+
+    return answer;
+  }
+
+
+  public static void main(String args[]){
+    Main T = new Main();
+
+    Scanner kb = new Scanner(System.in);
+    int prince = kb.nextInt();
+    int k = kb.nextInt();
+
+    System.out.println(T.solution(prince, k));
+  }
+}
