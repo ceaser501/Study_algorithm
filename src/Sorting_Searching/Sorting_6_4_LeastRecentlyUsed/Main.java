69d95476d857a3b6bdf4a3935d0dc78b722366d3
taesu.kim <ceaser501@naver.com>
2022-04-27 20:01:43 +0900

    LSU 진행중

---
+++ src/Sorting_Searching/Sorting_6_4_LeastRecentlyUsed/Main.java
@@ -0,0 +1,23 @@
+package Sorting_Searching.Sorting_6_4_LeastRecentlyUsed;
+
+// 정렬_Least Recently Used
+import java.util.*;
+
+public class Main {
+
+  public String solution(String str){
+    String answer = "";
+
+    return answer;
+  }
+
+
+  public static void main(String args[]){
+    Main T = new Main();
+
+    Scanner kb = new Scanner(System.in);
+    String str = kb.next();
+
+    System.out.println(T.solution(str));
+  }
+}
