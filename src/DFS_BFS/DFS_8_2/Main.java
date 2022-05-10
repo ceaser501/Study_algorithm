202bc1aad3a6bee61ceafe0f495350e2d1f55e7c
ceaser501 <ceaser501@naver.com>
2022-05-11 01:56:12 +0900

    바둑이 승차(DFS)

--- src/DFS_BFS/DFS_8_2/Main.java
+++ src/DFS_BFS/DFS_8_2/Main.java
@@ -1,24 +1,25 @@
 package DFS_BFS.DFS_8_2;
 
-// 바둑이 승차
+// 바둑이 승차_BFS
 import java.util.*;
 public class Main {
-
   static int w, n, answer = 0;
   public void DFS(int L, int sum, int[] arr){
-    if(sum > w){
-
+    if(w < sum) return;
+    if(L == n){
+      answer = Math.max(answer, sum);
     }else{
       DFS(L+1, sum+arr[L], arr);
+      DFS(L+1, sum, arr);
     }
-    
+
   }
 
   public static void main(String args[]){
     Main T = new Main();
     Scanner kb = new Scanner(System.in);
-    n = kb.nextInt();     // 바둑이 수
     w = kb.nextInt();     // 전체 싣을 수 있는 하중
+    n = kb.nextInt();     // 바둑이 수
     int[] arr = new int[n]; // 바둑이 정보
     for(int i=0; i<n; i++){
       arr[i] = kb.nextInt();
